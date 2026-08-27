c0a035e3a2f306c9ed8e2c6b55e8462993355734
Mark Counterman <ilovepoker@gmail.com>
2026-08-27 07:06:36 -0500

    Add Flarum extension bootstrap

---
+++ extend.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace FlatRate\SupabaseOAuth;
+
+use Flarum\Extend;
+use Flarum\User\Event\RegisteringFromProvider;
+use FoF\OAuth\Extend as OAuthExtend;
+
+return [
+    (new Extend\Frontend('forum'))
+        ->css(__DIR__.'/resources/less/forum.less'),
+
+    new Extend\Locales(__DIR__.'/resources/locale'),
+
+    new OAuthExtend\RegisterProvider(Providers\FlatRate::class),
+
+    (new Extend\Event())
+        ->listen(RegisteringFromProvider::class, Listeners\TrustVerifiedSupabaseEmail::class),
+
+    (new Extend\Middleware('forum'))
+        ->add(Middleware\RequireFlatRateIdentity::class),
+
+    (new Extend\Middleware('api'))
+        ->add(Middleware\RequireFlatRateIdentity::class),
+];
